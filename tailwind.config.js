3c5480ceacfd08256284df3a89b5b3fb1ac1f701
Pradeep rvs <37183050+Rwespradeep@users.noreply.github.com>
2023-05-16 19:07:52 +0530

    most optimized with design
    see all function and animations are pending

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'figtree': ['Figtree', 'sans-serif'],
+        'oswald': ['Oswald', 'sans-serif'],
+        'satisfy': ['Satisfy', 'cursive'],
+      },
+      colors: {
+        'font-grey': '#4F4F4F',
+      },
+      screens: {
+        'lg': '1030px',
+      }
+    },
+  },
+  plugins: [],
+}
+
